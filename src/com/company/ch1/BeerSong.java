a6e2d6a16b6b2f286c2390156695a1953aa3d2c4
Stasy-n <dacenko.anast@gmail.com>
2021-12-27 14:17:36 +0200

    changed folders

--- src/com/company/ch1/BeerSong.java
+++ src/com/company/ch1/BeerSong.java
@@ -1,3 +1,5 @@
+package com.company.ch1;
+
 public class BeerSong {
     public static void main(String[] args) {
         int beerNum = 99;
--- src/com/company/ch1/PhraseOMatic.java
+++ src/com/company/ch1/PhraseOMatic.java
@@ -1,3 +1,5 @@
+package com.company.ch1;
+
 public class PhraseOMatic { public static void main(String[] args) {
     String[] wordListOne = {"24/7", "multi-Tier", "30,000 foot", "B-to-B",
             "win-win", "front-end", "web-based", "pervasive", "smart", "six-sigma",
